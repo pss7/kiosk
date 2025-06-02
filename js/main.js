45e93e4898c2c2e465e54c6089ed9b07ee2b5c77
parksangseok <park0157@naver.com>
2025-06-02 15:20:50 +0900

    commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+$(function () {
+
+  $(".slickWrap .slick").slick({
+    variableWidth: false,
+    autoplay: true,
+    arrows: true,
+    dots: false,
+    accessibility: false,
+    draggable: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    zIndex: 1000,
+    pauseOnHover: false,
+    autoplaySpeed: 8000,
+    speed: 1500,
+    prevArrow: $('#section01 .prev'),
+    nextArrow: $('#section01 .next'),
+  });
+
+
+});
