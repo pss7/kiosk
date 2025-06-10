bdba6afa49e39f208204e45ad161ed6976a29f74
pss7 <124497122+pss7@users.noreply.github.com>
2025-06-10 10:46:13 +0900

    Add files via upload

---
+++ js/main02.js
@@ -0,0 +1,17 @@
+
+$(function () {
+
+  /*
+ $(window).on('load', function () {
+    setInterval(function () {
+      $('.animation').each(function () {
+        const $el = $(this);
+        $el.removeClass('effect');
+        void this.offsetWidth;
+        $el.addClass('effect');
+      });
+    }, 60000);
+  });
+*/
+
+});
--- js/sub.js
+++ js/sub.js
@@ -4,4 +4,22 @@ $(function () {
     $(".orgInfoBox").addClass("effect");
   });
 
+  $(".chuncheonViewBox .slickBox .slick").slick({
+    variableWidth: false,
+    autoplay: true,
+    arrows: true,
+    dots: false,
+    accessibility: false,
+    draggable: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    zIndex: 1000,
+    pauseOnHover: false,
+    autoplaySpeed: 8000,
+    speed: 1500,
+    prevArrow: $('.chuncheonViewBox .prev'),
+    nextArrow: $('.chuncheonViewBox .next'),
+  });
+
 });
