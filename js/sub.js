ca5bc856e751a8c8926693477aad81457ec904f2
parksangseok <park0157@naver.com>
2025-06-15 17:46:54 +0900

    commit

--- js/sub.js
+++ js/sub.js
@@ -22,11 +22,6 @@ $(function () {
     nextArrow: $('.chuncheonViewBox .next'),
   });
 
-
-
-
-
-
   // $(".chuncheonBox02 .slickWrap .slick").slick({
   //   autoplay: false,
   //   arrows: true,
@@ -46,11 +41,25 @@ $(function () {
   // $('.chuncheonBox02 .slickWrap').on('afterChange', function (init, event, slick, currentSlide, nextSlide) {
   //   $('.chuncheonBox02 .slickWrap .slide').removeClass('active');
   //   $('.chuncheonBox02 .slickWrap .slick-current').addClass('active');
-  // });
+  // });  
 
+  $('.chuncheonBox03 .tabContentBox .tabContent').hide();
+  $('.chuncheonBox03 .tabContentBox .tabContent').first().show();
+  $(".chuncheonBox03 .tabMenu li").click(function () {
 
+    $(".chuncheonBox03 .tabMenu li").children().removeClass("active");
+    $(this).children().addClass('active');
 
+    const idx = $(this).index();
 
+    $('.chuncheonBox03 .tabContentBox .tabContent').hide();
+    const $target = $('.chuncheonBox03 .tabContentBox .tabContent').eq(idx).show();
+
+    $target.find('.slick').slick('setPosition');
+    $target.find('.slideMain').slick('setPosition');
+    $target.find('.slideNav').slick('setPosition');
+
+  });
 
   $(".chuncheonBox03 .slideWrap .slick").slick({
     autoplay: false,
@@ -67,43 +76,48 @@ $(function () {
     speed: 1500,
   });
 
-$('.chuncheonBox03 .mainSlide').each(function () {
-  const $mainSlideWrap = $(this);
-  const $slideMain = $mainSlideWrap.find('.slideMain');
-  const $sliderNav = $mainSlideWrap.find('.slideNav');
-  const $thumbSlides = $sliderNav.find('.thumbSlide');
-
-  $sliderNav.slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: $slideMain,
-    variableWidth: true,
-    focusOnSelect: true,
-    arrows: false,
+  $('.chuncheonBox03 .controlBox .prevButton').on('click', function () {
+    $('.chuncheonBox03 .tabContent:visible .slick').slick('slickPrev');
   });
 
-  $slideMain.slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: $sliderNav,
-    arrows: true,
-    prevArrow: $mainSlideWrap.find('.control .prev'),
-    nextArrow: $mainSlideWrap.find('.control .next'),
+  $('.chuncheonBox03 .controlBox .nextButton').on('click', function () {
+    $('.chuncheonBox03 .tabContent:visible .slick').slick('slickNext');
   });
 
-  $slideMain.on('afterChange', function (event, slick, currentSlide) {
+  $('.chuncheonBox03 .mainSlide').each(function () {
+    const $mainSlideWrap = $(this);
+    const $slideMain = $mainSlideWrap.find('.slideMain');
+    const $sliderNav = $mainSlideWrap.find('.slideNav');
+    const $thumbSlides = $sliderNav.find('.thumbSlide');
+
+    $sliderNav.slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: $slideMain,
+      variableWidth: true,
+      focusOnSelect: true,
+      arrows: false,
+    });
+
+    $slideMain.slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: $sliderNav,
+      arrows: true,
+      prevArrow: $mainSlideWrap.find('.control .prev'),
+      nextArrow: $mainSlideWrap.find('.control .next'),
+    });
+
+    $slideMain.on('afterChange', function (event, slick, currentSlide) {
+      $thumbSlides.removeClass('is-selected');
+      $thumbSlides.eq(currentSlide).addClass('is-selected');
+
+      $sliderNav.slick('slickGoTo', currentSlide);
+    });
+
     $thumbSlides.removeClass('is-selected');
-    $thumbSlides.eq(currentSlide).addClass('is-selected');
-
-    $sliderNav.slick('slickGoTo', currentSlide);
+    $thumbSlides.eq(0).addClass('is-selected');
   });
-
-  $thumbSlides.removeClass('is-selected');
-  $thumbSlides.eq(0).addClass('is-selected');
-});
-
-
-
 
 
 });
